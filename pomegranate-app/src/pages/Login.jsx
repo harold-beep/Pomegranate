59d2993324c346ac50e9e15f0b24a1c3cb6b3e10
Jan Harold Dela Cruz <harolddc25@gmail.com>
2023-11-19 12:19:22 +0800

    setup file => prepare for tsx migration

---
+++ pomegranate-app/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Paper, Box } from "@mui/material";
+
+function Login() {
+  return (
+    <>
+      <Paper>Login will come soon!</Paper>
+      <Box>This feature will come for premium users</Box>
+    </>
+  );
+}
+
+export default Login;
